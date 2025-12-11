f0b248055e7a342c91d8743be400eacc16329d96
ayazbabar831 <ayazbabar831@gmail.com>
2025-12-11 14:26:54 +0500

    cleanign things

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cap = cv.VideoCapture(0)
 fps = 30
 delay = 1 / fps
 
-# hide cursor
+
 sys.stdout.write("\033[?25l")
 sys.stdout.flush()
 
@@ -26,32 +26,8 @@ while True:
 
     time.sleep(delay)
 
-# show cursor
-sys.stdout.write("\033[?25h")
-sys.stdout.flush()
-
-cap.release()
-cap = cv.VideoCapture(0)
-fps = 30
-delay = 1 / fps
-
-sys.stdout.write("\033[?25l")
-sys.stdout.flush()
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    
-    frame = resize_frame(frame, 120)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    ascii_img = frame_to_ascii(gray)
-
-    sys.stdout.write("\033[H")
-    sys.stdout.write(ascii_img)
-    sys.stdout.flush()
-    time.sleep(delay)
 
 sys.stdout.write("\033[?25h")
 sys.stdout.flush()
+
 cap.release()
